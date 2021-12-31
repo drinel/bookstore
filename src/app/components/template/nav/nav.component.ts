7f7d2f1f295842712f71b26db8cba582ed0a62b6
drinel <will.i.am.002@hotmail.com>
2021-12-31 15:59:36 -0300

    deploy

---
+++ src/app/components/template/nav/nav.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
+
+
+
+@Component({
+  selector: 'app-nav',
+  templateUrl: './nav.component.html',
+  styleUrls: ['./nav.component.css']
+})
+export class NavComponent implements OnInit {
+
+  
+
+  constructor() { 
+  }
+
+  
+  ngOnInit(): void {
+       
+    }
+  }
+
+
+
+
